b86f25a7189307c3d62c906d4d82cc28f58216d0
Samuel Sacco <SamLSacco@aol.com>
2021-05-10 22:51:44 -0400

    raised execption and stuck on initialize

---
+++ Error_handling/user_script.rb
@@ -0,0 +1,15 @@
+require_relative 'super_useful'
+begin
+    puts "'five' == #{convert_to_int('five')}"
+rescue => exception
+    
+end
+
+
+feed_me_a_fruit
+
+sam = BestFriend.new('', 1, '')
+
+sam.talk_about_friendship
+sam.do_friendstuff
+sam.give_friendship_bracelet
